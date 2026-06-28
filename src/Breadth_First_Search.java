b2108f04a566457c57511fbb4902e571a4860dbe
Yin You <yin.you@yahoo.com>
2026-06-28 16:36:10 -0500

    Depth_First_Search and Breadth_First_Search (but just concepts)

---
+++ src/Breadth_First_Search.java
@@ -0,0 +1,11 @@
+public class Breadth_First_Search {
+    public static void main(String args[]) {
+
+        // the opposite sort of compared to depth first search, it goes one level at a time
+
+    }
+
+    private static void bfs() {
+
+    }
+}
---
+++ src/Depth_First_Search.java
@@ -0,0 +1,18 @@
+public class Depth_First_Search {
+    public static void main(String args[]) {
+
+        // search algorithm for traversing a tree or graph data structure
+        /*
+        1. pick a route
+        2. keep going until you reach a dead end, or a previously visited node
+        3. backtrack to last node that has unvisited adjacent neighbors
+         */
+
+
+
+    }
+
+    private static void dfs() {
+
+    }
+}
