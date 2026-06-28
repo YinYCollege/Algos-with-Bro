a2c368099249d9a66b3674fc12875edf908b574b
Yin You <yin.you@yahoo.com>
2026-06-28 15:15:17 -0500

    Merge_Sort

--- src/Interpolation_Search.java
+++ src/Interpolation_Search.java
@@ -3,7 +3,7 @@ public class Interpolation_Search {
     public static void main(String args[]) {
 
         // interpolation search = improvement over binary search best used for uniformly distributed data
-        // guesses where a vlaue might be based on calculated probe results if probe is incorrect, search area is narrowed, and a new probe is calculated
+        // guesses where a value might be based on calculated probe results if probe is incorrect, search area is narrowed, and a new probe is calculated
         // runtime average: O(log(log(n)))
         // worst case: O(n)
 
