84ddecc1b0c170bc0c63c6a7c1bbd74a3ac142f8
Yin You <yin.you@yahoo.com>
2026-07-01 14:36:19 -0500

    DFS and BFS logic understood but no implementation

---
+++ Python/breadth.py
@@ -0,0 +1,11 @@
+# what is breadth first search? 
+# breadth first search is a searching algorithm, like the depth first search that traverses through a graph/tree, that travels every single path one level at a time
+# instead of going through an entire path until it reaches the end, it traverses through every single path once before continuing
+# uses queues
+
+def bfs():
+    pass
+
+def bfsHelper():
+    pass
+
---
+++ Python/depth.py
@@ -0,0 +1,12 @@
+# what is depth first search? 
+# this is a sorting algorithm, mainly on graphs and trees subsequently, that takes one route and follows through until it reaches a dead end or visits a node 
+# it has already visited, afterwards go back to a previously traveled node that has another path and continues until that path reaches a dead end or a already 
+# visited node and repeat
+# uses stacks
+
+def dfs():
+    pass
+
+def dfsHelper():
+    pass
+
